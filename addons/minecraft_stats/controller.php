11a9e6bf1b9ce494dc47207082e17ef955cc9e06
Iain Cuthbertson <bigcuthy@gmail.com>
2011-12-15 14:34:56 +0000

    Added total time connected to minecraft stats addon. Added display multiple minecraft user stats at once.

--- addons/minecraft_stats/controller.php
+++ addons/minecraft_stats/controller.php
@@ -50,6 +50,8 @@ class addonMinecraftStats extends botController
 	
 	public function manageLine(&$irc, &$data)
 	{
+		global $tigBase;
+		
 		if (in_array($data->nick, $this->friends))
 		{
 			// check that the line wasn't user chatter
@@ -81,6 +83,8 @@ class addonMinecraftStats extends botController
 	
 	private function recordConnection($botNick, $mcUser)
 	{
+		global $tigBase;
+		
 		$addConnectionSql = 'INSERT INTO ' . $this->getTablePrefix() . 'minecraft_stats (bot_name, user_nick, action) VALUES ("' . $this->mdb2->escape($botNick) . '", "' . $this->mdb2->escape($mcUser) . '", "connection");';
 		$queryResult = $this->mdb2->query($addConnectionSql);
 		
@@ -91,6 +95,8 @@ class addonMinecraftStats extends botController
 	
 	private function recordDisconnection($botNick, $mcUser, $reason)
 	{
+		global $tigBase;
+		
 		$addDisconnectionSql = 'INSERT INTO ' . $this->getTablePrefix() . 'minecraft_stats (bot_name, user_nick, action, notes) VALUES ("' . $this->mdb2->escape($botNick) . '", "' . $this->mdb2->escape($mcUser) . '", "disconnection", "' . $this->mdb2->escape($reason) . '");';
 		$queryResult = $this->mdb2->query($addDisconnectionSql);
 		
@@ -101,49 +107,136 @@ class addonMinecraftStats extends botController
 	
 	public function displayStats(&$irc, &$data)
 	{
+		global $tigBase;
+		
+		if ($data->channel != '')
+			$sendTo = $data->channel;
+		else
+			$sendTo = $data->nick;
+		
 		if (count($data->messageex) == 2)
 		{
-			// display stats for a user
-			$displayResults = array();
-			
-			if ($data->channel != '')
-				$sendTo = $data->channel;
-			else
-				$sendTo = $data->nick;
-			
-			$connectionStatsSql = 'SELECT COUNT(*) AS connections FROM ' . $this->getTablePrefix() . 'minecraft_stats WHERE user_nick = "' . $this->mdb2->escape($data->messageex[1]) . '" AND `action` = "connection";';
-			$queryResult = $this->mdb2->query($connectionStatsSql);
+			// single user stats required
+			$mcUser = $data->messageex[1];
+			$displayResults = $this->getSingleUserStats($mcUser);
+			$irc->message($data->type, $sendTo, "Minecraft stats for " . $mcUser . " -- " . implode(', ', $displayResults));
+		}
+		else
+		{
+			// all user stats required
+			$mcUsernamesSql = 'SELECT user_nick FROM ' . $this->getTablePrefix() . 'minecraft_stats GROUP BY user_nick ORDER BY user_nick ASC;';
+			$queryResult = $this->mdb2->query($mcUsernamesSql);
+			$mcUsers = array();
 			
 			if (PEAR::isError($queryResult)) {
-				$irc->message($data->type, $sendTo, "There was an error while fetching connection data on " . $$data->messageex[1]);
+				$irc->message($data->type, $sendTo, "There was an error while fetching connection data for all users");
 				return true;
 			}
 			
-			while ($queryResult && $statsRow = $queryResult->fetchRow(MDB2_FETCHMODE_ASSOC))
+			while ($queryResult && $userRow = $queryResult->fetchRow(MDB2_FETCHMODE_ASSOC))
 			{
-				$displayResults[] .= 'Connections: ' . $statsRow['connections'];
+				$mcUsers[] = $userRow['user_nick'];
 			}
 			
-			
-			$disconnectionStatsSql = 'SELECT COUNT(*) AS disconnections FROM ' . $this->getTablePrefix() . 'minecraft_stats WHERE user_nick = "' . $this->mdb2->escape($data->messageex[1]) . '" AND `action` = "disconnection";';
-			$queryResult = $this->mdb2->query($disconnectionStatsSql);
-			
-			if (PEAR::isError($queryResult)) {
-				$irc->message($data->type, $sendTo, "There was an error while fetching disconnection data on " . $$data->messageex[1]);
-				return true;
-			}
-			
-			while ($queryResult && $statsRow = $queryResult->fetchRow(MDB2_FETCHMODE_ASSOC))
+			foreach ($mcUsers as $mcUser)
 			{
-				$displayResults[] .= 'Disconnections: ' . $statsRow['disconnections'];
-			}
-			
-			
-			if (count($displayResults) > 0)
-			{
-				$irc->message($data->type, $sendTo, "Minecraft stats for " . $data->messageex[1] . " -- " . implode(', ', $displayResults));
+				$displayResults = $this->getSingleUserStats($mcUser);
+				$irc->message($data->type, $sendTo, "Minecraft stats for " . $mcUser . " -- " . implode(', ', $displayResults));
 			}
 		}
+	}
+	
+	
+	private function getSingleUserStats($mcUser)
+	{
+		global $tigBase;
+		
+		// display stats for a user
+		$displayResults = array();
+		
+	// -- calculate number of connections
+		$connectionStatsSql = 'SELECT COUNT(*) AS connections FROM ' . $this->getTablePrefix() . 'minecraft_stats WHERE user_nick = "' . $this->mdb2->escape($mcUser) . '" AND `action` = "connection";';
+		$queryResult = $this->mdb2->query($connectionStatsSql);
+		
+		if (PEAR::isError($queryResult)) {
+			$irc->message($data->type, $sendTo, "There was an error while fetching connection data on " . $mcUser);
+			return true;
+		}
+		
+		while ($queryResult && $statsRow = $queryResult->fetchRow(MDB2_FETCHMODE_ASSOC))
+		{
+			$displayResults[] = 'Connections: ' . $statsRow['connections'];
+		}
+		
+		
+		
+	// -- calculate number of disconnections
+		$disconnectionStatsSql = 'SELECT COUNT(*) AS disconnections FROM ' . $this->getTablePrefix() . 'minecraft_stats WHERE user_nick = "' . $this->mdb2->escape($mcUser) . '" AND `action` = "disconnection";';
+		$queryResult = $this->mdb2->query($disconnectionStatsSql);
+		
+		if (PEAR::isError($queryResult)) {
+			$irc->message($data->type, $sendTo, "There was an error while fetching disconnection data on " . $mcUser);
+			return true;
+		}
+		
+		while ($queryResult && $statsRow = $queryResult->fetchRow(MDB2_FETCHMODE_ASSOC))
+		{
+			$displayResults[] = 'Disconnections: ' . $statsRow['disconnections'];
+		}
+		
+		
+		
+	// -- calculate total time connected
+		$connectionTimesSql = 'SELECT * FROM ' . $this->getTablePrefix() . 'minecraft_stats WHERE user_nick = "' . $this->mdb2->escape($mcUser) . '" ORDER BY created_at ASC;';
+		$queryResult = $this->mdb2->query($connectionTimesSql);
+		
+		if (PEAR::isError($queryResult)) {
+			$irc->message($data->type, $sendTo, "There was an error while fetching connection data on " . $mcUser);
+			return true;
+		}
+		
+		$connectionTimePairs = array();
+		$counter = 0;
+		while ($queryResult && $connectionTimeRow = $queryResult->fetchRow(MDB2_FETCHMODE_ASSOC))
+		{
+			if (!is_array($connectionTimePairs[$counter]))
+				$connectionTimePairs[$counter] = array();
+			
+			if ($connectionTimeRow['action'] == 'connection')
+			{
+				$connectionTimePairs[$counter]['connection'] = $connectionTimeRow['created_at'];
+			}
+			if ($connectionTimeRow['action'] == 'disconnection')
+			{
+				// skip double disconnection entries
+				if (isset($connectionTimePairs[$counter]['disconnection']))
+					continue;
+				
+				$connectionTimePairs[$counter]['disconnection'] = $connectionTimeRow['created_at'];
+			}
+			
+			if (count($connectionTimePairs[$counter]) == 2)
+			{
+				$counter++;
+			}
+		}
+		
+		if (count($connectionTimePairs) > 0)
+		{
+			if (!isset($connectionTimePairs[count($connectionTimePairs) - 1]['disconnection']))
+			$connectionTimePairs[count($connectionTimePairs) - 1]['disconnection'] = date('Y-m-d H:i:s');
+		}
+		
+		$totalTimeConnected = 0;
+		foreach ($connectionTimePairs as $connectionTimePair)
+		{
+			$connectTime = strtotime($connectionTimePair['connection']);
+			$disconnectTime = strtotime($connectionTimePair['disconnection']);
+			$totalTimeConnected += ($disconnectTime - $connectTime);
+		}
+		$displayResults[] = 'Total time connected: ' .  $tigBase->sec2hms($totalTimeConnected, false);
+		
+		return $displayResults;
 	}
 }
 
